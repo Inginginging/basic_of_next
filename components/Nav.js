a87d2d4c064cabdf8eae813bc16c10b2771dc305
ing928 <kik928@nate.com>
2022-04-26 21:31:41 +0900

    Styling with styled jsc

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./Nav.module.css";
 
 export default function Nav() {
   //앱의 함수 컴포넌트에서 Router 객체 내부에 접근할 수 있게 해주는 hook
@@ -8,24 +7,22 @@ export default function Nav() {
   return (
     <nav>
       <Link href="/">
-        <a
-          /*css module로 className 두개를 넣는 방법. */
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/about" ? styles.active : ""
-          }`}
-        >
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: linen;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: blue;
+        }
+      `}</style>
     </nav>
   );
 }
