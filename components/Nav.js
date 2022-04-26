99db39e642cb21a19220038141ff81b167690169
ing928 <kik928@nate.com>
2022-04-26 21:18:24 +0900

    Styling with Css Module

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./Nav.module.css";
 
 export default function Nav() {
   //앱의 함수 컴포넌트에서 Router 객체 내부에 접근할 수 있게 해주는 hook
@@ -7,10 +8,21 @@ export default function Nav() {
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a
+          /*css module로 className 두개를 넣는 방법. */
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/about" ? styles.active : ""
+          }`}
+        >
           About
         </a>
       </Link>
