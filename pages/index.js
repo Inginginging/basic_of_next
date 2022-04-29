1e319b80e0cedf482baacd144298d30feb0e02b3
ing928 <kik928@nate.com>
2022-04-29 15:42:45 +0900

    getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
+  //server에서 미리 data를 받고 page에 랜더링하기 때문에 front에선 laoding을 보여줄 필요가 없음
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +34,17 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+//server에서 미리 data를 fetch 받아서 front의 page props로 넘겨주는 함수. 함수 이름은 무조건 getServerSideProps여야함.
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  //항상 객체를 return하고 props key를 가짐
+  return {
+    props: {
+      results,
+    },
+  };
 }
