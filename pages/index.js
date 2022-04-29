0772279bd291dd9cdfef2652f4b7a4271b2bc175
ing928 <kik928@nate.com>
2022-04-29 21:06:53 +0900

    router.push

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
   //server에서 미리 data를 받고 page에 랜더링하기 때문에 front에선 laoding을 보여줄 필요가 없음
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      //url이동 시 넘어가는 정보
+      {
+        pathname: `movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      //url 주소를 masking 해줌
+      `movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <Link href={`movies/${movie.id}`}>
+            <h4>{movie.original_title}</h4>
+          </Link>
         </div>
       ))}
       <style jsx>{`
@@ -17,6 +39,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
